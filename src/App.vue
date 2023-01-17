201adcaf93dc8e42a1e9c3c06f3d19d6213a6b96
PeterPH95 <1269894219@qq.com>
2023-01-17 19:53:21 +0800

    first push

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import ByteSlider from './components/byte-slider.vue'
 </script>
 
 <template>
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ByteSlider />
 </template>
 
 <style scoped>
---
+++ src/components/byte-slider.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="byte-slider">
+    <input
+      type="range"
+      :min="min"
+      :max="max"
+      :value="modelValue"
+      :step="step"
+      :disabled="disabled"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  min: {
+    type: Number,
+    default: 0,
+  },
+  max: {
+    type: Number,
+    default: 100,
+  },
+  modelValue: {
+    type: Number,
+    default: 0,
+  },
+  step: {
+    type: Number,
+    default: 1,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+
+</script>
+<style lang="less" scoped>
+.byte-slider {
+  // background-color: #ccc;
+
+  input {
+    width: 100%;
+    cursor: pointer;
+  }
+}
+</style>
