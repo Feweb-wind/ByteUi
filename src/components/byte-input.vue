0b63a8e1cc91725d57e565cb30d9922ddb520c53
常峰 <8845317+winlatom@user.noreply.gitee.com>
2023-01-17 11:16:51 +0800

    updata input by changfeng

--- src/components/byte-input.vue
+++ src/components/byte-input.vue
@@ -1,70 +1,102 @@
 <template>
-  <div class="byte-input">
-    <div class="byte-input__wrapper" :class="{ 'is-focus': isFocus }">
-      <input
-        class="byte-input__inner"
-        :value="props.value"
-        :type="props.type"
-        :placeholder="props.placeholder"
-        v-on:focusin="isFocus = true"
-        v-on:focusout="isFocus = false"
-      />
-    </div>
+  <div class="byte-input" :class="size">
+    <input
+    :type="type"
+    :value="modelValue"
+    :disabled="disabled"
+    :maxlength="maxlength"
+    :minlength="minlength"
+    :readonly="readonly"
+    :placeholder="placeholder"
+    :showWordLimit="showWordLimit"
+    @input="updateModelValue">
+    {{ showPassword }}
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-
+//props
 const props = defineProps({
-  value: {
+  //原生属性
+  type:{
+    type:String,
+    default:'text',
+    require:true
+  },
+  modelValue: {
     type: [String, Number],
     default: '',
   },
-  type: {
-    type: String,
-    required: false,
-    default: 'text',
+  disabled:{//是否禁用
+    type:Boolean,
+    defalut:false
   },
-  placeholder: {
-    type: String,
-    default: '',
+  maxlength:Number,
+  minlength:Number,
+  readonly:{
+    type:Boolean,
+    default:false
   },
+  placeholder:String,
+  showWordLimit:{
+    type:Boolean,
+    default:false
+  },
+  showPassword:{//是否切换密码图标
+    type:Boolean,
+    defalut:true
+  },
+  size:{//大小,medium / small / mini
+    type:String,
+    validator(value:string) {
+      // The value must match one of these strings
+      return ['medium', 'small', 'mini'].includes(value)
+    }
+  }
 })
+//emits
+const emit = defineEmits<{
+  (e:'update:modelValue',newvalue:[string,Number]):[string,Number] //双向绑定事件
+}>()
+//实现双向绑定
+const updateModelValue = (e:Event)=>{
+    emit('update:modelValue',e.target.value)
+}
 
 let isFocus = ref(false)
 </script>
 
 <style lang="less">
-.byte-input {
-  width: 100%;
-
-  input {
-    outline: none;
-    border: none;
+//大小,medium / small / mini
+  .medium[class="byte-input medium"]{
+  height: 36px;
   }
-
-  .byte-input__wrapper {
-    display: inline-flex;
-    flex-grow: 1;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    padding: 1px 11px;
-    box-sizing: border-box;
-    box-shadow: 0 0 0 1px #dcdfe6 inset;
-    transition: box-shadow 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+  .small[class="byte-input small"]{
+    height: 32px;
+  }
+  .mini[class="byte-input mini"]{
+    height: 28px;
+  }
+.byte-input{
+  width: 180px;
+  height: 40px;
+  input{
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: none;
     border-radius: 4px;
-  }
-
-  .byte-input__wrapper.is-focus {
-    box-shadow: 0 0 0 1px #409eff inset;
-  }
-
-  .byte-input__inner {
+    border: 1px solid #dcdfe6;
+    box-sizing: border-box;
+    color: #606266;
+    display: inline-block;
+    font-size: inherit;
+    height: 100%;
+    line-height: 100%;
+    outline: none;
+    padding: 0 15px;
+    transition: border-color .2s cubic-bezier(.645,.045,.355,1);
     width: 100%;
-    height: 30px;
-    font-size: 14px;
   }
 }
 </style>
