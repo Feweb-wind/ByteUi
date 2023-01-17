c61806e9421e557d46281760918a3e470d244109
Muzzle886 <2568682437@qq.com>
2023-01-17 23:02:59 +0800

    finish byte-input-number all function

--- src/components/byte-input-number.vue
+++ src/components/byte-input-number.vue
@@ -1,41 +1,137 @@
 <template>
-  <div class="byte-input-number">
-    <span class="byte-input-number__decrease" @click="decrease">
-      <i class="byte-icon">
-        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill="currentColor"
-            d="M128 544h768a32 32 0 1 0 0-64H128a32 32 0 0 0 0 64z"
-          ></path>
-        </svg>
-      </i>
+  <div
+    class="byte-input-number"
+    :class="{
+      'is-disabled': props.disabled,
+      'is-controls-right': props.controls && props.controlsPosition === 'right',
+      'byte-input-number--large': props.size && props.size === 'large',
+      'byte-input-number--small': props.size && props.size === 'small',
+    }"
+  >
+    <span
+      class="byte-input-number__decrease"
+      :class="{
+        'is-disabled': props.min && value <= props.min!,
+      }"
+      @click="decrease"
+    >
+      <i class="byte-icon"><Minus /></i>
     </span>
-    <ByteInput :value="value" />
-    <span class="byte-input-number__increase" @click="increase">
-      <i class="byte-icon">
-        <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-          <path
-            fill="currentColor"
-            d="M480 480V128a32 32 0 0 1 64 0v352h352a32 32 0 1 1 0 64H544v352a32 32 0 1 1-64 0V544H128a32 32 0 0 1 0-64h352z"
-          ></path>
-        </svg>
-      </i>
+    <span
+      class="byte-input-number__increase"
+      :class="{
+      'is-disabled': props.max && value >= props.max!,
+    }"
+      @click="increase"
+    >
+      <i class="byte-icon"><Plus /></i>
     </span>
+    <ByteInput
+      v-model="inuptValue"
+      type="number"
+      :placeholder="props.placeholder"
+      :name="props.name"
+      :disabled="props.disabled"
+      :size="props.size"
+      :readonly="props.readonly"
+      :max="props.max"
+      :min="props.min"
+      :label="props.label"
+      @change="valueChange(inuptValue)"
+      @blur="event => emits('blur', event)"
+      @focus="event => emits('focus', event)"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
 import ByteInput from './byte-input.vue'
-let value = ref(1)
-let setup = ref(5)
+import { Minus, Plus } from '@element-plus/icons-vue'
+
+export interface ByteInputNumber {
+  modelValue: number | undefined
+  max?: number
+  min?: number
+  setup?: number
+  stepStrictly?: boolean
+  precision?: number
+  size?: 'large' | 'default' | 'small'
+  readonly?: boolean
+  disabled?: boolean
+  controls?: boolean
+  controlsPosition?: 'right'
+  name?: string
+  label?: string
+  placeholder?: string
+  valueOnClear?: 'max' | 'min' | number | null
+  validateEvent?: boolean
+}
+
+const props = withDefaults(defineProps<ByteInputNumber>(), {
+  modelValue: 1,
+  setup: 1,
+  'step-strictly': false,
+  size: 'default',
+  readonly: false,
+  disabled: false,
+  controls: true,
+  validateEvent: true,
+})
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', newValue: number | undefined): number
+  (
+    e: 'change',
+    currentValue: number | undefined,
+    oldValue: number | undefined
+  ): void
+  (e: 'blur', event: Event): void
+  (e: 'focus', event: Event): void
+}>()
+
+let value = ref<number>(props.modelValue)
+let inuptValue = ref<number | string>(value.value)
 
 function decrease() {
-  value.value -= setup.value
+  if (!props.disabled && !props.readonly) valueChange(value.value - props.setup)
 }
 
 function increase() {
-  value.value += setup.value
+  if (!props.disabled && !props.readonly) valueChange(value.value + props.setup)
+}
+
+function valueChange(newValue: number | string) {
+  let oldValue = value.value
+  if (newValue === '') {
+    if (props.valueOnClear) {
+      if (
+        typeof props.valueOnClear === 'string' &&
+        ((props.valueOnClear === 'max' && props.max) ||
+          (props.valueOnClear === 'min' && props.min))
+      ) {
+        newValue = props.valueOnClear === 'max' ? props.max! : props.min!
+      }
+      if (typeof props.valueOnClear === 'number') {
+        newValue = props.valueOnClear
+      }
+    } else {
+      emits('update:modelValue', undefined)
+      return
+    }
+  }
+
+  value.value = typeof newValue === 'string' ? parseFloat(newValue) : newValue
+  if (props.max && value.value > props.max) {
+    value.value = props.max
+  } else if (props.min && value.value < props.min) {
+    value.value = props.min
+  } else if (props.stepStrictly && value.value % props.setup !== 0) {
+    valueChange(value.value - (value.value % props.setup))
+  }
+  inuptValue.value = value.value
+  emits('change', value.value, oldValue)
+  emits('update:modelValue', value.value)
 }
 </script>
 
@@ -75,6 +171,12 @@ function increase() {
     color: #409eff;
   }
 
+  .byte-input-number__decrease.is-disabled,
+  .byte-input-number__increase.is-disabled {
+    color: #a8abb2;
+    cursor: not-allowed;
+  }
+
   .byte-input-number__decrease {
     border-radius: 4px 0 0 4px;
     left: 1px;
@@ -86,12 +188,49 @@ function increase() {
     right: 1px;
     border-left: 1px solid #dcdfe6;
   }
-  .byte-input__wrapper {
-    padding-left: 42px;
-    padding-right: 42px;
+  .inner-input {
+    input {
+      text-align: center;
+    }
   }
-  .byte-input__inner {
-    text-align: center;
+}
+.byte-input-number.is-controls-right {
+  .byte-input-number__decrease,
+  .byte-input-number__increase {
+    height: 19px;
+  }
+  .byte-input-number__decrease {
+    left: auto;
+    top: auto;
+    right: 1px;
+    border-right: none;
+    border-left: 1px solid #dcdfe6;
+    border-radius: 0 0 4px 0;
+  }
+  .byte-input-number__increase {
+    border-radius: 0 4px 0 0;
+    border-bottom: 1px solid #dcdfe6;
+  }
+}
+.byte-input-number.byte-input-number--large.is-controls-right {
+  .byte-input-number__decrease,
+  .byte-input-number__increase {
+    height: 23px;
+  }
+}
+.byte-input-number.byte-input-number--small.is-controls-right {
+  .byte-input-number__decrease,
+  .byte-input-number__increase {
+    height: 15px;
+  }
+}
+.byte-input-number.is-disabled {
+  cursor: not-allowed;
+  .byte-input-number__decrease,
+  .byte-input-number__increase {
+    border-color: #e4e7ed;
+    color: #e4e7ed;
+    cursor: not-allowed;
   }
 }
 </style>
