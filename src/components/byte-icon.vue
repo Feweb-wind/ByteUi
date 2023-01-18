573085361b1632f79a74af0fa1e5b4cb8b221ad5
Muzzle886 <2568682437@qq.com>
2023-01-18 23:05:33 +0800

    finish icon

---
+++ src/components/byte-icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <i class="byte-icon" :style="style">
+    <slot />
+  </i>
+</template>
+
+<script lang="ts" setup>
+import { computed, CSSProperties } from 'vue'
+
+const props = defineProps<{
+  size?: number | string
+  color?: string
+}>()
+
+const style = computed<CSSProperties>(() => {
+  const { size, color } = props
+  if (!size && !color) return {}
+  return {
+    fontSize: size === 'undefined' ? undefined : addUnit(size!),
+    '--color': color,
+  }
+})
+
+function addUnit(size: number | string): string {
+  return typeof size === 'string' ? size : size + 'px'
+}
+</script>
+
+<style lang="less">
+.byte-icon {
+  height: 1em;
+  width: 1em;
+}
+</style>
