18033db79c69fcdc709a992dcb70a96c309e8ac1
goldSilence <15615529635@163.com>
2023-01-30 21:39:59 +0800

    change button style

--- docs/.vitepress/config/sidebar.ts
+++ docs/.vitepress/config/sidebar.ts
@@ -4,29 +4,30 @@ export default {
             text: 'Basic 基础组件',
             items: [
                 {text: 'Button 按钮', link: '/components/button'},
-                {text: 'Border 边框', link: '/components/border'},
+            ]
+        },
+        {
+            text: 'Form 表单组件',
+            items: [
+                {text: 'Select 选择器', link: '/components/select'},
             ]
         },
         {
             text: 'Data 数据展示',
             items: [
-                {text: 'Button 按钮', link: '/components/button'},
                 {text: 'Tag 标签', link: '/components/tag'},
-                {text: 'Tooltip 文字提示', link: '/components/tooltip'},
             ]
         },
         {
             text: 'Navigation 导航',
             items: [
-                {text: 'Button 按钮', link: '/components/button'},
                 {text: 'Border 边框', link: '/components/border'},
             ]
         },
         {
             text: 'Feedback 反馈组件',
             items: [
-                {text: 'Button 按钮', link: '/components/button'},
-                {text: 'Border 边框', link: '/components/border'},
+                {text: 'Tooltip 文字提示', link: '/components/tooltip'},
             ]
         },
         {
