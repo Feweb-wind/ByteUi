38286a2f85d5f162302e7d0dde7f7b1efcb1d45c
goldSilence <15615529635@163.com>
2023-01-30 15:38:49 +0800

    modify docs:dev

--- src/components/byte-button-group/index.ts
+++
@@ -1,6 +0,0 @@
-import {withInstall} from "@byte-ui/utils";
-import ButtonGroup from "../byte-button/src/button-group.vue";
-export const ByteButtonGroup = withInstall(ButtonGroup);
-
-export default ByteButtonGroup;
-export * from "../byte-button/src/button-group.vue";
