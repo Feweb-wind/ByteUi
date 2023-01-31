ca11d2432ada325a64a612a8711ad76081e9c332
xing <1725256426@qq.com>
2023-01-31 20:56:13 +0800

    finish message

--- src/utils/type.ts
+++ src/utils/type.ts
@@ -1,4 +1,7 @@
 export const isElement = (e: unknown): e is Element => {
-    if (typeof Element === 'undefined') return false
-    return e instanceof Element
-}
+  if (typeof Element === "undefined") return false;
+  return e instanceof Element;
+};
+
+export { isNumber, isString, isFunction } from "lodash-unified";
+export { isVNode } from "vue";
---
+++ src/utils/vue/icon.ts
@@ -0,0 +1,22 @@
+import {
+  CircleCloseFilled,
+  InfoFilled,
+  SuccessFilled,
+  WarningFilled,
+  Close,
+} from "@element-plus/icons-vue";
+
+export const TypeComponents = {
+  Close,
+  SuccessFilled,
+  InfoFilled,
+  WarningFilled,
+  CircleCloseFilled,
+};
+
+export const TypeComponentsMap = {
+  success: SuccessFilled,
+  warning: WarningFilled,
+  error: CircleCloseFilled,
+  info: InfoFilled,
+};
